16cf6dedceeacf7272ffdacd58e50d0f26466467
MustafaShf <itsmustafashafique@gmail.com>
2024-08-26 00:24:29 +0500

    class-01

---
+++ 1/timeComplexity.cpp
@@ -0,0 +1,8 @@
+//! Time Complexity Tips
+
+// Tips to Remember
+
+//     *Loops: A single loop that goes from 0 to n generally has a time complexity of O(n).
+//     *Nested Loops: Two nested loops over the same range usually mean O(n^2).
+//     *Halving the Problem Size: If the algorithm cuts the problem size in half each time (like binary search), it's O(log n).
+//     *Ignoring Constants: Constants and smaller terms are ignored in Big-O notation. Focus on the largest growing factor.
